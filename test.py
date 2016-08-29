68d2e4b96e4d862d9d3425ef1c4532a780726e6c
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-08-29 16:20:50 +0800

    change demo script to normalize

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ import clean
 start_time = time.clock()
 
 # clean the corpus
-path_in = '/home/nhanh/TDT2_top20'
-path_out = '/home/nhanh/TDT2_top20_cleaned'
-corpus.corpus_clean(path_in,path_out)
+# path_in = '/home/nhanh/TDT2_top20'
+# path_out = '/home/nhanh/TDT2_top20_cleaned'
+# corpus.corpus_clean(path_in,path_out)
 
 # normalize the corpus
-# nlp = process.initialize()
-# path_in = '/home/nhanh/20096'
-# path_out = '/home/nhanh/20096_coref'
-# corpus.corpus_normalize(path_in,path_out,nlp)
+nlp = process.initialize()
+path_in = '/home/nhanh/TDT2_top20'
+path_out = '/home/nhanh/TDT2_top20_coref'
+corpus.corpus_normalize(path_in,path_out,nlp)
 
 print(time.clock() - start_time)
