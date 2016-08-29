34b5b594ae3c2e6bd39d25624f1e4e1c72af18a6
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-08-30 02:27:15 +0800

    remove unused imports and code

--- clean.py
+++ clean.py
@@ -1,7 +1,4 @@
 import re
-# import string
-# import os
-# import glob
 
 KNOWN_REPLACEMENTS = [
 	('``','"'), ("''",'"'), ('_','-')
@@ -28,62 +25,3 @@ def clean(txt):
 	for m in methods:
 		txt = m(txt)
 	return txt
-
-# def remove_formula(string):
-# 	#function count punctuation
-# 	count = lambda l1,l2: sum([1 for x in l1 if x in l2])
-	
-# 	num_comma = string.count(',')
-# 	num_fullstop = 0
-# 	for c in string.split(' '):
-# 		if c.endswith('.'):
-# 		# count omly full stop at end of a word (full stop in middle of a word may be a float like 3.2)
-# 			num_fullstop += 1
-# 	if (num_comma + num_fullstop) > 0:
-# 	# only return string with , or .
-# 		return string
-# 	else:
-# 		return ''
-
-# def string_validation(string):
-# 	string = unicode(string, errors='ignore')
-# 	'''
-# 	if string != '\n':
-# 		return string
-# 	else:
-# 		return ''
-# 	'''
-# 	if string == '\n' or string.startswith(',') or string.startswith('.'):
-# 		return ''
-# 	else:
-# 		return string
-
-# # if a line contains only two-, one, or zero-character sequences, it is most definitely a formula/ whitespace/ non-text
-# twocharseq = re.compile(r'[a-z]{3,}',re.I)
-# # further formulas, those ending with annotations e.g. (1.3) and starting with =
-# annot = re.compile(r'\(\d+(.\d+)+\)\n')
-# equals = re.compile(r'=[\w\s+-=<>()|]+\n')
-# # figures and tables
-# fig = re.compile(r'figure[\w\s+-=<>()]+\n',re.I)
-# tab = re.compile(r'table[\w\s+-=<>()]+\n',re.I)
-# # chapter headings
-# ch = re.compile(r'\d+(.\d+)+[\w\s+-=<>()]+\n',re.I)
-
-
-# for line in file_in:
-# 	if (twocharseq.search(line) and not annot.search(line) and not equals.match(line)):
-# 		if (ch.match(line)):
-# 			text_out+=('\n'+line)
-# 		elif (fig.match(line)):
-# 			text_out+='<FIG>\n'
-# 		elif (tab.match(line)):
-# 			text_out+='<TAB>\n'
-# 		else:
-# 			text_out+=(line)
-# 	line_out = string_validation(remove_formula(line))
-# 	if (not fig.match(line_out)):
-# 		text_out+=line_out
-
-# file_out = open('output.txt','w')
-# file_out.write(text_out)
-# file_out.close()
