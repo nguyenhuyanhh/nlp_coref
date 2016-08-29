0fd711fc1e60c264e4dd24fa3017e6525dd3e6ce
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-08-29 15:03:23 +0800

    change demo script to clean

--- test.py
+++ test.py
@@ -5,10 +5,17 @@ import process
 import corpus
 import clean
 
-nlp = process.initialize()
-path_in = '/home/nhanh/20096'
-path_out = '/home/nhanh/20096_coref'
-
 start_time = time.clock()
-corpus.corpus_normalize(path_in,path_out,nlp)
-print(time.clock()-start_time)
+
+# clean the corpus
+path_in = '/home/nhanh/TDT2_top20'
+path_out = '/home/nhanh/TDT2_top20_cleaned'
+corpus.corpus_clean(path_in,path_out)
+
+# normalize the corpus
+# nlp = process.initialize()
+# path_in = '/home/nhanh/20096'
+# path_out = '/home/nhanh/20096_coref'
+# corpus.corpus_normalize(path_in,path_out,nlp)
+
+print(time.clock() - start_time)
