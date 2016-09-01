b81794204f853466af24abe9bcd65471e533fa47
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-09-01 16:28:20 +0800

    add normalize to demo

--- demo.py
+++ demo.py
@@ -6,12 +6,20 @@ from corpus import Corpus
 import clean
 
 init_start_time = time.time()
+
+# inits
 p = Process()
 c = Corpus(p)
+
 init_end_time = time.time()
 print('Init time: ' + str(init_end_time - init_start_time) + ' seconds')
 
 start_time = time.time()
+
+# normalize some text
+# text_in = open('input.txt','r').read()
+# p.normalize()
+
 # clean the corpus
 # path_in = '/home/nhanh/TDT2_top20'
 # path_out = '/home/nhanh/TDT2_top20_cleaned'
@@ -21,6 +29,6 @@ start_time = time.time()
 path_in = '/home/nhanh/test'
 path_out = '/home/nhanh/test_coref'
 c.corpus_normalize_m(path_in,path_out)
-end_time = time.time()
 
+end_time = time.time()
 print('Run time: ' + str(end_time - start_time) + ' seconds')
