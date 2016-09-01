5e0bd5a5fbf5acd1aee93b6b41a4eb6a5dd4cc5d
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-09-01 17:12:59 +0800

    pep8

--- clean.py
+++ clean.py
@@ -1,27 +1,30 @@
 import re
 
 KNOWN_REPLACEMENTS = [
-	('``','"'), ("''",'"'), ('_','-')
+    ('``', '"'), ("''", '"'), ('_', '-')
 ]
 
+
 def known_replacements(_str):
-	for pair in KNOWN_REPLACEMENTS:
-		_str = _str.replace(pair[0],pair[1])
-	return _str
+    for pair in KNOWN_REPLACEMENTS:
+        _str = _str.replace(pair[0], pair[1])
+    return _str
 
 
 def remove_multiple_whitespace(_str):
-	return re.sub(r'\s+', ' ', _str).strip()
+    return re.sub(r'\s+', ' ', _str).strip()
+
 
 def mult_to_singleline(txt):
-	txt = remove_multiple_whitespace(txt)
-	return ' '.join(txt.split('\n')).strip()
+    txt = remove_multiple_whitespace(txt)
+    return ' '.join(txt.split('\n')).strip()
+
 
 def clean(txt):
-	methods = [
-		known_replacements,
-		mult_to_singleline
-	]
-	for m in methods:
-		txt = m(txt)
-	return txt
+    methods = [
+        known_replacements,
+        mult_to_singleline
+    ]
+    for m in methods:
+        txt = m(txt)
+    return txt
