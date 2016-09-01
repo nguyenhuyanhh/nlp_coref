89a356ed592f66dd7178fe0a078296bbd819f2d1
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-09-01 17:15:28 +0800

    pep8

--- demo.py
+++ demo.py
@@ -23,12 +23,12 @@ start_time = time.time()
 # clean the corpus
 # path_in = '/home/nhanh/TDT2_top20'
 # path_out = '/home/nhanh/TDT2_top20_cleaned'
-# c.corpus_clean(path_in,path_out)
+# c.corpus_clean(path_in, path_out)
 
 # normalize the corpus
 path_in = '/home/nhanh/test'
 path_out = '/home/nhanh/test_coref'
-c.corpus_normalize_m(path_in,path_out)
+c.corpus_normalize_m(path_in, path_out)
 
 end_time = time.time()
 print('Run time: ' + str(end_time - start_time) + ' seconds')
