34c648eaf1f0275eeebfea120da7aa5408b856d2
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-09-01 16:24:07 +0800

    pretty output for init time and run time

--- demo.py
+++ demo.py
@@ -5,11 +5,13 @@ from process import Process
 from corpus import Corpus
 import clean
 
+init_start_time = time.time()
 p = Process()
 c = Corpus(p)
+init_end_time = time.time()
+print('Init time: ' + str(init_end_time - init_start_time) + ' seconds')
 
 start_time = time.time()
-
 # clean the corpus
 # path_in = '/home/nhanh/TDT2_top20'
 # path_out = '/home/nhanh/TDT2_top20_cleaned'
@@ -19,7 +21,6 @@ start_time = time.time()
 path_in = '/home/nhanh/test'
 path_out = '/home/nhanh/test_coref'
 c.corpus_normalize_m(path_in,path_out)
-
 end_time = time.time()
 
-print(end_time - start_time)
+print('Run time: ' + str(end_time - start_time) + ' seconds')
