c3ecb655ca6fa6a279e945ba1b07af0b30a0dffe
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-09-20 07:12:27 +0800

    test dsp terms model

--- coref/clean.py
+++ coref/clean.py
@@ -1,14 +1,29 @@
 import re
 
 KNOWN_REPLACEMENTS = [
-    ('``', '"'), ("''", '"'), ('_', '-')
+    ('``', '"'), ("''", '"'), ('_', '-'), ('ﬁ', 'fi'), ('ﬀ', 'ff'), 
+    ('ﬂ', 'fl'), ('ﬃ', 'ffi'), ('ﬄ', 'ffl')
 ]
 
+# helper functions
+def load_model(file):
+    model = dict()
+    with open(file, 'r') as model_file:
+        for line in model_file:
+            tmp = line.split(maxsplit=1)
+            model[tmp[0]] = tmp[1].rstrip('\n')
+    return model
 
+
+# rules
 def known_replacements(_str):
     for pair in KNOWN_REPLACEMENTS:
         _str = _str.replace(pair[0], pair[1])
     return _str
+
+
+def remove_non_ascii(_str):
+    return re.sub(r'[^\x00-\x7F]', '', text)
 
 
 def remove_multiple_whitespace(_str):
@@ -20,9 +35,11 @@ def mult_to_singleline(txt):
     return ' '.join(txt.split('\n')).strip()
 
 
+# main function
 def Clean(txt):
     methods = [
         known_replacements,
+        remove_non_ascii,
         mult_to_singleline
     ]
     for m in methods:
--- demo.py
+++ demo.py
@@ -1,14 +1,15 @@
 # demo script
 import time
+import os
 
-from coref.process import Process
+# from coref.process import Process
 # from coref.corpus import Corpus
-from coref.clean import Clean
+from coref.clean import Clean, load_model
 
 init_start_time = time.time()
 
 # inits
-p = Process()
+# p = Process()
 # c = Corpus(p)
 
 init_end_time = time.time()
@@ -17,8 +18,8 @@ print('Init time: ' + str(init_end_time - init_start_time) + ' seconds')
 start_time = time.time()
 
 # normalize some text
-text_in = open('data/input.txt','r').read()
-print(Clean(p.normalize(Clean(text_in))))
+# text_in = open('data/input.txt','r').read()
+# print(Clean(p.normalize(Clean(text_in))))
 
 # clean the corpus
 # path_in = '/home/nhanh/TDT2_top20'
@@ -29,6 +30,25 @@ print(Clean(p.normalize(Clean(text_in))))
 # path_in = '/home/nhanh/test'
 # path_out = '/home/nhanh/test_coref'
 # c.corpus_normalize_m(path_in, path_out)
+
+# test the model
+model = load_model('./coref/dsp_terms.txt')
+data_dirs = [7, 8, 9, 10, 11]
+for dir in data_dirs:
+    count_files = 0
+    count_occur = 0
+    path = './data/raw_' + str(dir)
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            count_files += 1
+            file = os.path.join(root, f)
+            with open(file, 'r') as fi:
+                text = fi.read()
+                for key in model.keys():
+                    if key in text:
+                        count_occur += 1
+                        break
+    print('{}: {} {}'.format(dir, count_files, count_occur)) 
 
 end_time = time.time()
 print('Run time: ' + str(end_time - start_time) + ' seconds')
