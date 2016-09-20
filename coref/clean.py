477e053b2bcf4a5bd3a90d2c053ed9b30fccb69e
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-09-21 07:02:57 +0800

    new cleaner and demo

--- coref/clean.py
+++ coref/clean.py
@@ -1,11 +1,37 @@
 import re
+import random
+import os
 
 KNOWN_REPLACEMENTS = [
     ('``', '"'), ("''", '"'), ('_', '-'), ('ﬁ', 'fi'), ('ﬀ', 'ff'), 
     ('ﬂ', 'fl'), ('ﬃ', 'ffi'), ('ﬄ', 'ffl')
 ]
 
+# regex patterns
+formula_pattern = r'\b([ωδ\w]+ ?\([\w ωδ]+\)|[^\s]* =( ?[+−]?\w+([\.,]\d+)? ?)([+−×*]( ?\w+(\.\d+)? ?))*( ?= ?[+−]?\d+(\.\d+)?)?\b|zzz)'
+expression_pattern = r'\b( ?[+−]?\w+([\.,]\d+)? ?)([+−×*/]( ?\w+(\.\d+)? ?))+( ?= ?[+−]?\d+(\.\d+)?)?\b'
+range_pattern =  r'−?(\d+|\w|∞)? [≤≥><] −?(\d+|\w|∞)'
+
 # helper functions
+def count_endingfullstop(_str):
+    num_fullstop = 0
+    for word in _str.strip().split():
+        if word.endswith('.'):
+            num_fullstop += 1
+    return num_fullstop
+
+
+def is_formula(_str):
+    if re.search(r'[A-Za-z]{2,}', _str):
+        return False
+    else:
+        return True
+
+
+def remove_multiple_whitespace(_str):
+    return re.sub(r'\s+', ' ', _str).strip()
+
+
 def load_model(file):
     model = dict()
     with open(file, 'r') as model_file:
@@ -23,16 +49,66 @@ def known_replacements(_str):
 
 
 def remove_non_ascii(_str):
-    return re.sub(r'[^\x00-\x7F]', '', text)
+    return re.sub(r'[^\x00-\x7F]', '', _str)
 
 
-def remove_multiple_whitespace(_str):
-    return re.sub(r'\s+', ' ', _str).strip()
+def string_validation(_str):
+    lines = list()
+    for line in _str.splitlines():
+        if line == '\n' or line.startswith(',') or line.startswith('.') or len(line.split()) == 1:
+            lines.append('')
+        else:
+            lines.append(line)
+    return '\n'.join(lines)
 
 
-def mult_to_singleline(txt):
-    txt = remove_multiple_whitespace(txt)
-    return ' '.join(txt.split('\n')).strip()
+def remove_formula(_str):
+    lines = list()
+    for line in _str.splitlines():
+        count = lambda l1, l2: sum([1 for x in l1 if x in l2])
+        num_comma = line.count(',')
+        num_fullstop = count_endingfullstop(line)
+        have_mathoperation = re.search(r'[+−×≤<>≥=≈]+', line)
+    
+        if (num_comma + num_fullstop) > 0:
+            lines.append(line)
+        else:
+            if not have_mathoperation:
+                if is_formula(line):
+                    lines.append('[FORMULA]')
+                else:
+                    lines.append(line)
+            else:
+                lines.append('[FORMULA]')
+    return '\n'.join(lines)
+
+
+def remove_inline_formula(_str):
+    lines = list()
+    for line in _str.splitlines():
+        have_formula = re.search(formula_pattern, line)
+        have_range = re.search(range_pattern, line)
+        if have_formula:
+            string_noformula = re.sub(formula_pattern, 'zzz', line)
+            string_nobracket = re.sub(r'[()]+', '', string_noformula)
+            string_noformula = re.sub(formula_pattern, '[FORMULA]', string_nobracket)
+            if re.search(expression_pattern, string_noformula):
+                string_noformula = re.sub(expression_pattern, '[FORMULA]', string_noformula)
+            if have_range:
+                string_noformula = re.sub(range_pattern, '[FORMULA]', string_noformula)
+            lines.append(string_noformula)
+        if have_range:
+            lines.append(re.sub(range_pattern, '[FORMULA]', line))
+    return '\n'.join(lines)
+
+
+def join_broken_words(_str):
+    return re.sub(r'\b- \b', '', _str)
+
+
+def mult_to_single_line(_str):
+    _str = remove_multiple_whitespace(_str)
+    return ' '.join(_str.split('\n')).strip()
 
 
 # main function
@@ -40,7 +116,11 @@ def Clean(txt):
     methods = [
         known_replacements,
         remove_non_ascii,
-        mult_to_singleline
+        string_validation,
+        remove_formula,
+        remove_inline_formula,
+        join_broken_words,
+        mult_to_single_line
     ]
     for m in methods:
         txt = m(txt)
--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 # demo script
 import time
 import os
+import string
+import random
+import re
 
 # from coref.process import Process
 # from coref.corpus import Corpus
@@ -36,20 +39,35 @@ model = load_model('./coref/dsp_terms.txt')
 data_dirs = [7, 8, 9, 10, 11]
 for dir in data_dirs:
     count_files = 0
-    count_occur = 0
+    count_files_occur = 0
+    count_words_occur = 0
     path = './data/raw_' + str(dir)
     for root, dirs, files in os.walk(path):
         for f in files:
             file = os.path.join(root, f)
             with open(file, 'r') as fi:
-                text = fi.read()
-                if (not text == ''):
+                words = re.split('\W+', fi.read())
+                count_words_occur_pre = count_words_occur
+                if (len(words) > 1):
                 	count_files += 1 
                 for key in model.keys():
-                    if key in text:
-                        count_occur += 1
-                        break
-    print('{}: {} {}'.format(dir, count_files, count_occur)) 
+                    if key in words:
+                        count_words_occur += 1
+                if (count_words_occur > count_words_occur_pre):
+                	count_files_occur += 1        
+    print('Book {}: {} sections, {} sections with terms, {} terms'.format(dir, count_files, count_files_occur, count_words_occur))
+
+# test the cleaner
+# path = './data/raw_7'
+# for root, dirs, files in os.walk(path):
+#     f = random.choice(files)
+#     print(f)
+#     print()
+#     with open(os.path.join(root,f), 'r') as file:
+#         txt = file.read()
+#         print(txt)
+#         print()
+#         print(Clean(txt))
 
 end_time = time.time()
 print('Run time: ' + str(end_time - start_time) + ' seconds')
