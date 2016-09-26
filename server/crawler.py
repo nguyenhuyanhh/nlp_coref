6632873f5689985cec2075843c37ecd065bba1e8
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-09-26 12:21:04 +0800

    improve: crawler

--- server/crawler.py
+++ server/crawler.py
@@ -6,13 +6,16 @@ from pdf_client import config
 from pdf_client.multithread.processor import TextProcessor
 from pdf_client.multithread.worker import MultiThreadWorker
 
-if not os.path.isdir("../data/raw_10"):
-    os.makedirs("../data/raw_10")
+# set book id
+book_id = 11
+book_dir = '../data/raw_{}'.format(book_id)
+if not os.path.isdir(book_dir):
+    os.makedirs(book_dir)
 
-# download all sections in a book into individual files
+# download all sections from the book into individual files
 class SectionDownloader(TextProcessor):
     def process(self, text, section_id):
-        with open("../data/raw_10/{id}.txt".format(id=section_id), 'w+') as file:
+        with open(os.path.join(book_dir, '{}.txt'.format(section_id)), 'w+') as file:
             file.write(text)
         return text
 
@@ -25,7 +28,7 @@ def main():
     config.load_from_file('./config.json')
 
     # create a worker and start
-    worker = MultiThreadWorker(processor=SectionDownloader(), book=10, threads=20)
+    worker = MultiThreadWorker(processor=SectionDownloader(), book=book_id)
     worker.start()
 
 if __name__ == '__main__':
