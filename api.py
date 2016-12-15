6f582a4c28bb903f72e39d405f71932aafabd028
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-12-16 07:27:47 +0800

    refactor api

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+"""api: API wrapper for coref.* and optionals.*"""
+
+import logging
 from time import time
 
 from coref.process import Process
@@ -7,46 +10,57 @@ from optionals.svm import svm_eval
 from optionals.crawler import book_download_raw_all
 
 # inits
-init_start_time = time()
-proc = Process()
-corp = Corpus(proc)
-init_end_time = time()
-print('Init time: ' + str(init_end_time - init_start_time) + ' seconds')
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+INIT_START = time()
+PROC = Process()
+CORP = Corpus(PROC)
+INIT_END = time()
+LOG.info('Init time: %s seconds', INIT_END - INIT_START)
 
 
 def clean(txt, debug=0):
+    """optionals.clean.Clean().clean"""
     cleaner = Clean()
     return cleaner.clean(txt, debug)
 
 
 def clean_non_ascii(txt, debug=0):
+    """optionals.clean.Clean([]).clean"""
     cleaner = Clean([known_replacements, remove_non_ascii])
     return cleaner.clean(txt, debug)
 
 
 def annotate(txt):
-    return proc.annotate_txt(txt)
+    """coref.process.Process().annotate_txt"""
+    return PROC.annotate_txt(txt)
 
 
 def coref(txt):
-    return proc.coref_print(txt)
+    """coref.process.Process().coref_print"""
+    return PROC.coref_print(txt)
 
 
 def normalize(txt):
-    return proc.normalize(txt)
+    """coref.process.Process().normalize"""
+    return PROC.normalize(txt)
 
 
 def corpus_clean(path_in, path_out):
-    return corp.corpus_clean(path_in, path_out)
+    """coref.corpus.Corpus().corpus_clean"""
+    return CORP.corpus_clean(path_in, path_out)
 
 
 def corpus_normalize(path_in, path_out):
-    return corp.corpus_normalize(path_in, path_out)
+    """coref.corpus.Corpus().corpus_normalize"""
+    return CORP.corpus_normalize(path_in, path_out)
 
 
 def svm(path):
+    """optionals.svm.svm_eval"""
     svm_eval(path)
 
 
 def download_all():
+    """optionals.crawler.book_download_raw_all"""
     book_download_raw_all()
